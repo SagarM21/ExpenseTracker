8793fea0f4836a94c75e8e00365f0fe743ef5ee0
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-05-11 19:38:18 +0530

    add: alertDialog

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -40,6 +40,30 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsValid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsValid ||
+        _selectedDate == null) {
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Invalid input'),
+                content: const Text(
+                    'Please make sure a valid title, amount, date and category was entered.'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(ctx);
+                      },
+                      child: const Text('Okay'))
+                ],
+              ));
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -95,10 +119,7 @@ class _NewExpenseState extends State<NewExpense> {
                 }),
             const Spacer(),
             ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense')),
             const SizedBox(
               width: 20,
